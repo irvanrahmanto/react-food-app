017cf3494db718baec6e525900dd1ce9b7287cd3
irvan <irvan.rahmanto@hanabank.co.id>
2023-10-05 10:31:25 +0700

    fix: fixing typo

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -26,7 +26,7 @@ const cartReducer = (state, action) => {
             updatedItems = [...state.items];
             updatedItems[existingCartItemIndex] = updatedItem;
         } else {
-            updatedItems = state.item.concat(action.item);
+            updatedItems = state.items.concat(action.item);
         }
 
         return {
